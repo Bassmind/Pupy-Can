95d535fb5ef463c7a846910b36b387cd4fa17189
David <isc.david.mendiola@hotmail.com>
2012-12-01 22:38:01 -0600

    Add User and category models

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Category do
+	it 'has a valid factory' do
+		build(:category).should be_valid
+	end
+
+  context 'with valid attributes' do
+  	let(:category) { create(:category) }
+
+  	it 'has a name' do
+  		category.name.should_not be_nil
+  	end
+  end
+
+  context 'with invalid attributes' do
+  	it 'with no name' do
+  		subject { build(:category, name: nil) }
+
+  		subject.should_not be_valid
+  	end
+  end
+end

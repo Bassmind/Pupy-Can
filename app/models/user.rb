ceb27bb70ba1b4cf57fa8969d1cb525b8469d8bf
David <isc.david.mendiola@hotmail.com>
2012-12-15 10:43:31 -0600

    Add tests to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :telefono
+  attr_accessible :email, :name, :phone
+  
+  validate :only_one_user, :on => :create
+  validates :name, :phone, :email, presence: true
+
+  private
+  def only_one_user
+  	if (User.count > 0)
+  		errors[:base] << "Ya hay un usuario registrado"
+  	end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+  	create(:user).should be_valid
+  end
+
+  it 'only allows one user in the database' do
+  	create(:user)
+  	build(:user).should_not be_valid
+  end
+
+  it 'is invalid without name' do
+  	build(:user, name: nil).should_not be_valid
+  end
+
+  it 'is invalid without phone' do
+  	build(:user, phone: nil).should_not be_valid
+  end
+
+  it 'is invalid without email' do
+  	build(:user, email: nil).should_not be_valid
+  end
 end

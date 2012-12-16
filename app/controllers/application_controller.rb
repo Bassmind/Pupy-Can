85044e37aba9469dc28a0bc013fdc89d3ae659b8
David <isc.david.mendiola@hotmail.com>
2012-12-15 23:28:33 -0600

    Add first test of admin controller. Deny access to guest user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def authorize
+  	if(User.count > 0)
+  		if ((session[:user] == User.first.name) && (session[:password] == User.first.password))
+	  		return true
+	  	end
+  	end
+
+  	reset_session
+  	flash[:notice] = "Password invalido"
+	  redirect_to root_path
+	  false
+  end
 end
--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe AdminController do
-
-  describe "GET 'index'" do
-    it "returns http success" do
+  describe "INDEX" do
+    it "when user is a guest, redirects to home page" do
       get 'index'
-      response.should be_success
+      response.should redirect_to root_path
     end
   end
-
 end

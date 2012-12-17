700666f9d3ddef5ece897eebaf370512dcfd9ad6
David <isc.david.mendiola@hotmail.com>
2012-12-16 18:20:23 -0600

    Add encryptation for session variables

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,13 @@
+require 'bcrypt'
+
 class LoginController < ApplicationController
   def index
   end
 
   def check
-  	session[:password] = params[:password]
-  	session[:name] = params[:name]
-  	
+  	session[:password] = BCrypt::Password.create(params[:password])
+  	session[:name] = BCrypt::Password.create(params[:name])
+    
   	if(authorize)
   		redirect_to admin_path
   	end

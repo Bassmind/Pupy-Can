e07b4f5eb6d2d497bdf3c400f3854c095eb0bf43
David <isc.david.mendiola@hotmail.com>
2013-01-16 17:51:33 -0600

    Modify token to fix an issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Profe::Application.config.secret_token = 'b084445ca3ea77153cd2e33fec56d69d8d91c596de751495d886223649236eff9c1d1a36bc1ca25ccafaaa8ef8b3a3e221204a6d4d3b9be139a862aa24ce2424'
+Profe::Application.config.secret_token = 'b054445ca3ea77153cd2e33fec56d69d8d91c596de751495d886223649236eff9c1d1a36bc1ca25ccafaaa8ef8b3a3e221204a6d4d3b9be139a862aa24ce2424'
